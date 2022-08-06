2a62c365e1509ee38eb035a22dbea4fce3133898
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-08-06 18:47:05 +0900

    add lower_bound

--- lib/data-structure/binary_indexed_tree.cpp
+++ lib/data-structure/binary_indexed_tree.cpp
@@ -6,6 +6,10 @@ using namespace std;
     binary_indexed_tree<T>(n) : BITをサイズnで構築
     add(i, x) : i 番目の要素に加算します。 O(log(n))
     sum(l, r) : [l, r) の区間和を取得します。O(log(n))
+    
+    (以下の機能を使うには、0 <= i < N において、A_i >= 0 である必要があります。)
+    lower_bound(x) : A_0 + A_1 + ... + A_y >= x となる最小の y を求めます。O(log(n))
+    upper_bound(x) : A_0 + A_1 + ... + A_y > x となる最小の y を求めます。O(log(n))
 */
 
 template <typename T>
@@ -14,23 +18,58 @@ struct binary_indexed_tree{
     vector<T> BIT;
     binary_indexed_tree(int N): N(N), BIT(N + 1, 0){
     }
+ 
     void add(int i, T x){
         i++;
-        while (i <= N){
+        while(i <= N){
             BIT[i] += x;
             i += i & -i;
         }
     }
+ 
     T sum(int i){
         T ans = 0;
-        while (i > 0){
+        while(i > 0){
             ans += BIT[i];
             i -= i & -i;
         }
         return ans;
     }
+ 
     T sum(int L, int R){
         return sum(R) - sum(L);
+    }
+ 
+    int lower_bound(T x){
+        if(x <= 0){
+            return 0;
+        }else{
+            int v = 0, r = 1;
+            while(r < N) r = r << 1;
+            for(int len = r; len > 0; len = len >> 1){
+                if(v + len < N && BIT[v + len] < x){
+                    x -= BIT[v + len];
+                    v += len;
+                }
+            }
+            return v;
+        }
+    }
+ 
+    int upper_bound(T x){
+        if(x < 0){
+            return 0;
+        }else{
+            int v = 0, r = 1;
+            while(r <= N) r = r << 1;
+            for(int len = r; len > 0; len = len >> 1){
+                if(v + len <= N && BIT[v + len] <= x){
+                    x -= BIT[v + len];
+                    v += len;
+                }
+            }
+            return v;
+        }
     }
 };
 
