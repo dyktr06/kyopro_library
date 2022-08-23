b9f3521794f2a3744b63c036e4674f2cf656b35a
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-08-24 05:06:57 +0900

    Create convexhulltrick.cpp

---
+++ lib/data-structure/convexhulltrick.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <typename T>
+struct ConvexHullTrick{
+    deque<pair<T, T>> deq;
+
+    ConvexHullTrick(): deq(){
+    }
+
+    bool check(pair<T, T> line1, pair<T, T> line2, pair<T, T> line3){
+        return (line2.first - line1.first) * (line3.second - line2.second) >= (line2.second - line1.second) * (line3.first - line2.first);
+    }
+
+    T f(pair<T, T> line, T x){
+        return line.first * x + line.second;
+    }
+
+    void add(T a, T b){
+        pair<T, T> p = {a, b};
+        while((int) deq.size() >= 2 && check(deq.at((int) deq.size() - 2), deq.at((int) deq.size() - 1), p)){
+            deq.pop_back();
+        }
+        deq.push_back(p);
+    }
+
+    T query(T x){
+        while((int) deq.size() >= 2 && f(deq.at(0), x) >= f(deq.at(1), x)){
+            deq.pop_front();
+        }
+        return f(deq.at(0), x);
+    }
+};
