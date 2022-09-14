2103c2fb68d66948a657d44b7406b84bc887848f
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-09-14 18:04:02 +0900

    combination faster

--- lib/math/modcalc.cpp
+++ lib/math/modcalc.cpp
@@ -31,34 +31,19 @@ T modinv(T a, T m){
 
 // mod. m での二項係数などを高速で計算します : O(NlogN)
 struct Combination{
-    vector<long long> memo;
-    vector<long long> memoinv;
+    vector<long long> memo, memoinv, inv;
     long long mod;
 
-    Combination(int N, long long m) : memo(N+1), memoinv(N+1){
+    Combination(int N, long long m) : memo(N + 1), memoinv(N + 1), inv(N + 1){
         mod = m;
-        memo[0] = 1;
-        for(int i = 1; i <= N; i++){
-            memo[i] = memo[i-1] * i;
-            memo[i] %= mod;
+        memo[0] = memo[1] = 1;
+        memoinv[0] = memoinv[1] = 1;
+        inv[1] = 1;
+        for(int i = 2; i <= N; i++){
+            memo[i] = memo[i - 1] * i % mod;
+            inv[i] = mod - inv[mod % i] * (m / i) % mod;
+            memoinv[i] = memoinv[i - 1] * inv[i] % mod;
         }
-        memoinv[0] = inv(1LL, mod);
-        for(int i = 1; i <= N; i++){
-            memoinv[i] = inv(1LL * memo[i], mod);
-            memoinv[i] %= mod;
-        }
-    }
-
-    long long inv(long long a, long long m){
-        long long b = m, u = 1, v = 0;
-        while (b) {
-            long long t = a / b;
-            a -= t * b; swap(a, b);
-            u -= t * v; swap(u, v);
-        }
-        u %= m;
-        if (u < 0) u += m;
-        return u;
     }
 
     long long fact(long long n){
