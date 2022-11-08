b77296b92dc52b12446b9949c285f84d86e0fad8
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-11-08 22:18:00 +0900

    add fraction operator

--- lib/math/fraction.cpp
+++ lib/math/fraction.cpp
@@ -4,23 +4,25 @@ using namespace std;
 
 // 分数(p/q) の構造体
 struct fraction{
-    long long p, q;
+    long long p, q; // long long or __int128_t
     fraction(long long P = 0, long long Q = 1): p(P), q(Q){
         normalize();
     }
     void normalize(){
         long long g = __gcd(p, q);
         p /= g, q /= g;
-        if(q < 0){
-            p *= -1, q *= -1;
-        }
+        if(q < 0) p *= -1, q *= -1;
     }
-    bool operator<(const fraction &other)const{
+    inline bool operator<(const fraction &other) const {
         return p * other.q < other.p * q;
     }
-    bool operator<=(const fraction &other)const{
-        return p * other.q >= other.p * q;
+    inline bool operator<=(const fraction &other) const {
+        return p * other.q <= other.p * q;
     }
+    inline fraction operator+(const fraction &other) const { return fraction(p * other.q + q * other.p, q * other.q); }
+    inline fraction operator-(const fraction &other) const { return fraction(p * other.q - q * other.p, q * other.q); }
+    inline fraction operator*(const fraction &other) const { return fraction(p * other.p, q * other.q); }
+    inline fraction operator/(const fraction &other) const { return fraction(p * other.q, q * other.p); }
 };
 
 void example(){
