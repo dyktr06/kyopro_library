80186c01b673623813f1433c125f16aeeff12951
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-11-08 22:18:21 +0900

    add shortest path

--- lib/graph/dijkstra.cpp
+++ lib/graph/dijkstra.cpp
@@ -9,7 +9,7 @@ vector<vector<array<long long, 2>>> G;
 template <typename T>
 vector<long long> dijkstra(T x){
     const long long INF = 0x1fffffffffffffff;
-    vector<long long> cost(n, INF);
+    vector<long long> cost((int) G.size(), INF);
     using P = pair<long long, long long>;
     priority_queue<P, vector<P>, greater<>> q;
     cost[x] = 0;
@@ -27,6 +27,44 @@ vector<long long> dijkstra(T x){
         }
     }
     return cost;
+}
+
+// s から t への最短パスを求めます : O(ElogV)
+pair<long long, vector<pair<int, int>>> shortestPath(int s, int t){
+    const long long INF = 0x1fffffffffffffff;
+    vector<long long> cost((int) G.size(), INF);
+    vector<int> par((int) G.size(), -1);
+    using P = pair<long long, long long>;
+    priority_queue<P, vector<P>, greater<>> q;
+    cost[s] = 0;
+    par[s] = -1;
+    q.emplace(0, s);
+
+    while(q.size()){
+        auto [c, at] = q.top();
+        q.pop();
+        if(c > cost[at]) continue;
+        for(auto& [to, t] : G[at]){
+            if(cost[to] > c + t){
+                par[to] = at;
+                cost[to] = c + t;
+                q.emplace(cost[to], to);
+            }
+        }
+    }
+
+    if(cost[t] == INF){
+        return {-1, {}};
+    }
+    vector<pair<int, int>> path;
+    int now = t;
+    while(par[now] != -1){
+        path.push_back({par[now], now});
+        now = par[now];
+    }
+    reverse(path.begin(), path.end());
+
+    return {cost[t], path};
 }
 
 // example
