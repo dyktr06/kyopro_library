4f963e726aab1d4fa8f565a55511b31138b6e7d3
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-11-11 04:52:59 +0900

    fix warshall_floyd

--- lib/graph/warshall_floyd.hpp
+++ lib/graph/warshall_floyd.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-// ワ―シャルフロイド法で全頂点間の最短距離を求めます : (O(n^3))
+/*
+    ワ―シャルフロイド法で全頂点間の最短距離を求めます : (O(n^3))
+*/
 template <typename T>
 void warshall_floyd(vector<vector<T>> &d, T INF) {
     int n = d.size();
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             for(int k = 0; k < n; k++){
-                if(d[j][i] == INF && d[i][k] == INF) continue;
+                if(d[j][i] == INF || d[i][k] == INF) continue;
                 d[j][k] = min(d[j][k], d[j][i] + d[i][k]);
             }
         }
