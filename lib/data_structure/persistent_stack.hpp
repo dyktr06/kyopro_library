1bf0dc569b8faa5f360de3ea4745bb442f34643a
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-11-23 22:11:31 +0900

    add persistent stack

---
+++ lib/data_structure/persistent_stack.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+template<typename T>
+struct PersistentStack{
+    T val;
+    PersistentStack *next = nullptr;
+    int exist = 0;
+
+    PersistentStack(const T &_val, PersistentStack *_next) : val(_val), next(_next){
+        exist = _next->exist + 1;
+    }
+
+    PersistentStack(){
+    }
+
+    int size() const {
+        return exist;
+    }
+
+    bool empty() const {
+        return size() == 0;
+    }
+
+    T top() const {
+        return val;
+    }
+
+    PersistentStack *pop() const {
+        return next;
+    }
+    
+    PersistentStack *push(const T &x){
+        return new PersistentStack(x, this);
+    }
+};
