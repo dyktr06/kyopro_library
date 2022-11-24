24388c5f50560100fc84259ad0d11131946ff137
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2022-11-24 20:00:57 +0900

    add test

---
+++ test/aoj/dsl/dsl_1_b.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_1_B"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../../lib/data_structure/weighted_union_find.hpp"
+
+int main(){
+    int n, q; cin >> n >> q;
+    WeightedUnionFind<int> tree(n);
+    while(q--){
+        int t; cin >> t;
+        if(t == 0){
+            int x, y, z; cin >> x >> y >> z;
+            tree.unite(x, y, z);
+        }else{
+            int x, y; cin >> x >> y;
+            if(tree.same(x, y)){
+                cout << tree.diff(x, y) << "\n";
+            }else{
+                cout << "?" << "\n";
+            }
+        }
+    }
+}
