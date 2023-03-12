9d0f17c880ec678fc75c665ae00926c129063dc8
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2023-03-12 15:40:27 +0900

    update arithmetic, geometric sum

--- docs/math/modcalc.md
+++ docs/math/modcalc.md
@@ -8,12 +8,14 @@ Mod に関する計算のアルゴリズムの詰め合わせです。
 
 - `modcalc::modpow(x, n, m)`: $x^n$ (mod. m) を計算します。
 - `modcalc::modinv(a, m)`: $a$ の逆元 (mod. m) を計算します。
-- `modcalc::modarithmeticsum(a, m)`: 初項 $a$, 公差 $d$, 項数 $n$ の等差数列の和 (mod. m) を計算します。
-- `modcalc::modgeometricsum(a, m)`: 初項 $a$, 公比 $r$, 項数 $n$ の等比数列の和 (mod. m) を計算します。
+- `modcalc::modarithmeticsum(a, d, n, m)`: 初項 $a$, 公差 $d$, 項数 $n$ の等差数列の和 (mod. m) を計算します。（任意 mod で計算できます）
+- `modcalc::modgeometricsum(a, r, n, m)`: 初項 $a$, 公比 $r$, 項数 $n$ の等比数列の和 (mod. m) を計算します。（素数 mod のみで計算できます）
+- `modcalc::modgeometricsum2(a, r, n, m)`: 初項 $a$, 公比 $r$, 項数 $n$ の等比数列の和 (mod. m) を計算します。（任意 mod で計算できます）
 
 #### 計算量
 
 - `modcalc::modpow(x, n, m)`: $\mathrm{O}(\log n)$
 - `modcalc::modinv(a, m)`: $\mathrm{O}(\log m)$
-- `modcalc::modarithmeticsum(a, m)`: $\mathrm{O}(\log m)$
-- `modcalc::modgeometricsum(a, m)`: $\mathrm{O}(\log nm)$
+- `modcalc::modarithmeticsum(a, d, n, m)`: $\mathrm{O}(1)$
+- `modcalc::modgeometricsum(a, r, n, m)`: $\mathrm{O}(\log nm)$
+- `modcalc::modgeometricsum2(a, r, n, m)`: $\mathrm{O}(\log n^2)$
--- lib/math/modcalc.hpp
+++ lib/math/modcalc.hpp
@@ -34,11 +34,12 @@ namespace modcalc{
 
     template <typename T>
     T modarithmeticsum(T a, T d, T n, T m){
-        a %= m, n %= m, d %= m;
-        T b = (n - 1) * d % m;
-        return n * (a * 2 + b) % m * modinv((T) 2, m) % m;
+        T m2 = m * 2;
+        a %= m2, n %= m2, d %= m2;
+        T b = (n - 1) * d % m2;
+        return ((n * (a * 2 + b) % m2) / 2) % m;
     }
-    
+
     template <typename T>
     T modgeometricsum(T a, T r, T n, T m){
         a %= m;
@@ -47,5 +48,26 @@ namespace modcalc{
             return a * n % m;
         }
         return a * (modpow(r, n, m) + m - 1) % m * modinv(r - 1, m) % m;
+    }
+
+    template <typename T>
+    T modgeometricsum2(T a, T r, T n, T m){
+        a %= m;
+        if(r == 1){
+            n %= m;
+            return a * n % m;
+        }
+        T ret = 0;
+        T x = 1 % m;
+        T sum = 0;
+        for(int i = 0; n > 0; ++i){
+            if(n & 1){
+                (ret += x * modpow(r, sum, m) % m) %= m;
+                sum |= 1LL << i;
+            }
+            (x += x * modpow(r, 1LL << i, m) % m) %= m;
+            n >>= 1;
+        }
+        return a * ret % m;
     }
 }
