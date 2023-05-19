0da70a2d313d0b3560eacf55cca4ac5023f07f99
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2023-05-19 15:09:06 +0900

    add test rabin_miller

---
+++ test/library_checker/math/primarity_test.test.cpp
@@ -0,0 +1,20 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/primarity_test"
+#include <bits/stdc++.h>
+using namespace std;
+
+#include "../../../lib/math/rho.hpp"
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int q; cin >> q;
+    while(q--){
+        long long n; cin >> n;
+        if(Rho::rabin_miller(n)){
+            cout << "Yes" << "\n";
+        }else{
+            cout << "No" << "\n";
+        }
+    }
+}
