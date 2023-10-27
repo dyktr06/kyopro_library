64abac481e03d7be8d535e94b783a91fb589af3e
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2023-10-27 17:53:33 +0900

    add val_range

---
+++ lib/others/val_range.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+// 0-indexed, (l, i], [i, r)
+template<typename T>
+pair<vector<int>, vector<int>> val_range(const vector<T> &v, bool is_min){
+    pair<vector<int>, vector<int>> res;
+    int n = v.size();
+    res.first.assign(n, -1);
+    res.second.assign(n, n);
+    stack<int> st;
+    st.push(-1);
+    for(int i = 0; i < n; ++i){
+        while(st.top() != -1){
+            if(is_min ^ (v[st.top()] < v[i])){
+                res.second[st.top()] = i;
+                st.pop();
+            }else{
+                break;
+            }
+        }
+        res.first[i] = st.top();
+        st.push(i);
+    }
+    return res;
+}
