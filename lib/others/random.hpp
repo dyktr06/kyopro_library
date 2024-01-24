dc80077733d1e816f1e2b85d34e24614239412b3
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2024-01-24 14:02:37 +0900

    update random.hpp

--- lib/others/random.hpp
+++ lib/others/random.hpp
@@ -1,9 +1,9 @@
+#pragma once
+
 #include <bits/stdc++.h>
-#include <windows.h>
 
 using namespace std;
 
-// 乱数生成
 struct RandomNumber{
     int64_t seed = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch()).count();
     uniform_int_distribution<long long> p;
@@ -12,16 +12,9 @@ struct RandomNumber{
 	RandomNumber(long long l = 0, long long r = 0){
         rnd = mt19937_64(seed);
         p = uniform_int_distribution<long long>(l, r);
-        Sleep(1);
     }
 
 	long long get(){
         return p(rnd);
     }
 };
-
-// example (1から100の乱数を生成)
-int main(){
-    RandomNumber r(1, 100);
-    cout << r.get() << endl;
-}
