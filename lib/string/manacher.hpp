8ad9770b09c7472c1223396d6e17f72e1ccb84c6
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2024-04-07 03:10:19 +0900

    update manacher

--- lib/string/manacher.hpp
+++ lib/string/manacher.hpp
@@ -11,7 +11,7 @@ vector<int> manacher(const T &s){
     vector<int> rad(n);
     int i = 0, j = 0;
     while(i < n){
-        while(i - j >= 0 && i + j < s.size() && s[i - j] == s[i + j]){
+        while(i - j >= 0 && i + j < (int) s.size() && s[i - j] == s[i + j]){
             j++;
         }
         rad[i] = j;
@@ -25,3 +25,26 @@ vector<int> manacher(const T &s){
     }
     return rad;
 }
+
+struct PalindromeCheck{
+    const string s;
+    string t;
+    int n;
+    vector<int> mana;
+
+    PalindromeCheck(const string &str) : s(str){
+        n = s.size();
+        t = "$";
+        for(int i = 0; i < n; i++){
+            t += s[i];
+            t += "$";
+        }
+        mana = manacher(t);
+    }
+
+    // [l, r)
+    bool isPalindrome(int l, int r) const {
+        int mid = l + r;
+        return r * 2 - mid - 1 < mana[mid];
+    }
+};
