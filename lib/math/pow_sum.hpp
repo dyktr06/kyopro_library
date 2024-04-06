b00c6ebde2e8a81367eb1a070ab6ac0a5b0db855
dyktr_06 <64836409+darumakka2@users.noreply.github.com>
2024-04-07 03:10:45 +0900

    update combination

--- lib/math/pow_sum.hpp
+++ lib/math/pow_sum.hpp
@@ -15,23 +15,44 @@ struct Combination{
             memoinv[i] = memoinv[i - 1] * inv[i];
         }
     }
-    inline T fact(const long long n) const {
+    inline T fact(const int n) const {
         return memo[n];
     }
-    inline T factinv(const long long n) const {
+    inline T factinv(const int n) const {
         return memoinv[n];
     }
-    inline T ncr(const long long n, const long long r) const {
+    inline T ncr(const int n, const int r) const {
         if(n < r || r < 0) return 0;
         return (memo[n] * memoinv[r]) * memoinv[n - r];
     }
-    inline T npr(const long long n, const long long r) const {
+    inline T npr(const int n, const int r) const {
         if(n < r || r < 0) return 0;
         return memo[n] * memoinv[n - r];
     }
-    inline T nhr(const long long n, const long long r) const {
+    // 重複組み合わせ
+    inline T nhr(const int n, const int r) const {
         if(n == 0 && r == 0) return 1;
         return ncr(n + r - 1, r);
+    }
+    // ボールの数、一個以上必要な箱の数、制限がない箱の数 (箱区別あり)
+    // a = 0 の場合は重複組み合わせ
+    inline T choose(const int n, const int a, const int b = 0) const {
+        if(n == 0) return !a;
+        return ncr(n + b - 1, a + b - 1);
+    }
+    // +1 n 個, -1 m 個, 累積和 >= 0
+    inline T cataran(const int n, const int m) const {
+        return ncr(n + m, n) - ncr(n + m, n - 1);
+    }
+    // +1 n 個, -1 m 個, 累積和 > -k
+    inline T cataran(const int n, const int m, const int k) const {
+        if(m < k) return ncr(n + m, n);
+        if(m < n + k) return ncr(n + m, n) - ncr(n + m, m - k);
+        return 0;
+    }
+    // +1 n 個, -1 m 個, 累積和 < +k
+    inline T cataran2(const int n, const int m, const int k) const {
+        return cataran(m, n, k);
     }
 };
 
