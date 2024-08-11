182adfcd48aa5729d7b0cd30c7745d47974e0a37
dyktr_06 <darumaka0625@gmail.com>
2024-08-11 14:50:42 +0900

    refactor binarybasis

--- lib/math/binarybasis.hpp
+++ lib/math/binarybasis.hpp
@@ -8,37 +8,63 @@
 template <typename T>
 struct BinaryBasis{
 
-    vector<T> basis, original;
+    vector<T> basis, original, inds;
+    int cnt = 0;
     BinaryBasis(const vector<T> &vec){
         for(auto x : vec){
+            T v = (T) 1 << (cnt++);
             T y = x;
-            for(auto z : basis){
-                y = min(y, y ^ z);
+            int len = basis.size();
+            for(int i = 0; i < len; i++){
+                if((y ^ basis[i]) < y){
+                    y ^= basis[i];
+                    v ^= inds[i];
+                }
             }
             if(y > 0){
                 basis.push_back(y);
                 original.push_back(x);
+                inds.push_back(v);
             }
         }
         normalize();
     }
 
-    void add(const T &x){
+    pair<T, T> add(const T &x){
+        T v = (T) 1 << (cnt++);
         T y = x;
-        for(auto z : basis){
-            y = min(y, y ^ z);
+        int len = basis.size();
+        for(int i = 0; i < len; i++){
+            if((y ^ basis[i]) < y){
+                y ^= basis[i];
+                v ^= inds[i];
+            }
         }
         if(y > 0){
             basis.push_back(y);
             original.push_back(x);
+            inds.push_back(v);
         }
+        return make_pair(y, v);
     }
 
     void normalize(){
-        sort(basis.begin(), basis.end());
+        int len = basis.size();
+        for(int i = 0; i < len; i++){
+            for(int j = i + 1; j < len; j++){
+                if(basis[j] < basis[i]){
+                    swap(basis[i], basis[j]);
+                    swap(original[i], original[j]);
+                    swap(inds[i], inds[j]);
+                }
+            }
+        }
         for(int i = 0; i < (int) basis.size(); ++i){
             for(int j = 0; j < i; ++j){
-                basis[i] = min(basis[i], basis[i] ^ basis[j]);
+                if((basis[i] ^ basis[j]) < basis[i]){
+                    basis[i] ^= basis[j];
+                    inds[i] ^= inds[j];
+                }
             }
         }
     }
