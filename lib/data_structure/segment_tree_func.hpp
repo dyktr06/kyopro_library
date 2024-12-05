ad9f8a59a25a03398c43e6c57a49eed6d4bc65c3
dyktr_06 <darumaka0625@gmail.com>
2024-12-06 00:58:38 +0900

    feat min_left, max_right

--- lib/data_structure/segment_tree_func.hpp
+++ lib/data_structure/segment_tree_func.hpp
@@ -5,12 +5,12 @@
 
 template <typename T, T (*op)(T, T), T(*e)()>
 struct SegTree{
-    int n;
+    int _n, n;
     std::vector<T> dat;
 
-    SegTree(int n_){
+    SegTree(int _n) : _n(_n) {
         int x = 1;
-        while(n_ > x){
+        while(_n > x){
             x *= 2;
         }
         n = x;
@@ -19,7 +19,7 @@ struct SegTree{
             dat[i] = e();
         }
     }
-    SegTree(std::vector<T> &v){
+    SegTree(std::vector<T> &v) : _n((int) v.size()) {
         int x = 1;
         while((int) v.size() > x){
             x *= 2;
@@ -68,5 +68,55 @@ public:
             r >>= 1;
         }
         return op(vl, vr);
+    }
+
+    template <class F>
+    int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if(l == _n) return _n;
+        l += n;
+        T sm = e();
+        do{
+            while(l % 2 == 0) l >>= 1;
+            if(!f(op(sm, dat[l]))){
+                while(l < n){
+                    l = (2 * l);
+                    if(f(op(sm, dat[l]))){
+                        sm = op(sm, dat[l]);
+                        l++;
+                    }
+                }
+                return l - n;
+            }
+            sm = op(sm, dat[l]);
+            l++;
+        }while((l & -l) != l);
+        return _n;
+    }
+
+    template <class F>
+    int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if(r == 0) return 0;
+        r += n;
+        T sm = e();
+        do{
+            r--;
+            while(r > 1 && (r % 2)) r >>= 1;
+            if(!f(op(dat[r], sm))){
+                while(r < n){
+                    r = (2 * r + 1);
+                    if(f(op(dat[r], sm))){
+                        sm = op(dat[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - n;
+            }
+            sm = op(dat[r], sm);
+        }while((r & -r) != r);
+        return 0;
     }
 };
